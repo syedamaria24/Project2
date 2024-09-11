37bb10cee40e03a3b0b17664cb7266c06e6d086c
unknown <maharukhyousuf@gmail.com>
2024-09-11 23:34:43 +0500

    add files

---
+++ app.js
@@ -0,0 +1,130 @@
+//  CHAPTER # 6
+
+// Q1:
+var x = 5;
+x = ++x;
+console.log(x);
+
+// Q2:
+var x = 100;
+console.log(--x);
+
+// Q3:
+var x = 50;
+var y = x++
+console.log(y)
+
+// Q4:
+var y = 50;
+var z = --y;
+console.log(z)
+
+// Q5:
+var num = 10;
+var newNum = num--;
+console.log(num); 
+
+// Q6:
+var num = 20;
+var originalvalue = num++
+console.log(num);
+
+// Q7:
+// var num = 8;
+// var newvalue = num++;
+    // alert("the new value is: " + num);
+
+
+// ;Q1 / CHAPTER # 7
+
+// Q1:
+var calculatedNum = 2+(2 * 6);
+var valueofcalculatedNum = 2 + 12;
+console.log("value of calculatedNum is: " + calculatedNum);
+
+// Q2:
+var calculatedNum = (2 + 2) * 6;
+var vlueofcalculatedNum = 4 * 6;
+console.log("value of calculatedNum is: " + calculatedNum);
+
+// Q3:
+var calculatedNum = (2 + 2) * (2 +2);
+var valueofcalculatedNum = 4 * 4;
+console.log("value of calculatedNum is: " + calculatedNum);
+
+// Q4:
+var calculatedNum = ((2 + 2) * 4) + 2;
+var valuecalculatedNum = 16 + 2;
+console.log("value of calculatedNUm is: " + calculatedNum);
+
+// Q5:
+var cost = (2 + 2) * (4 + 10);
+console.log("Value of cost: " + cost);
+
+// Q6:
+var units = 2 + (2 * 4) + 10;
+console.log("value of units: " + units);
+
+// Q7:
+var pressure = 4 / (2 * 4);
+console.log("value of pressure: " + pressure);
+
+
+// CHAPTER # 8(CONCATENATING TEXT STRING)
+
+// Q1:
+// var num = "2" + "2";
+// console.log("value of num is: " + num);
+
+// // Q2:
+// var message = ("Hello, " + "Dolly");
+// alert(message);
+
+// // Q3:
+
+// alert("33" + 3);
+
+// // Q4:
+// alert("Pakistan" + " " + "Zindabad");
+
+// Q5:
+var fname = "Hello";
+var lname = "World";
+console.log(fname + " " + lname);
+
+// CHAPTER # 9(PROMPTS)
+
+// Q1:
+var firstName = prompt("Enter first name");
+console.log("First name entered:", firstName);
+
+// Q2:
+var country = prompt("Country?", "China");
+console.log("Country entered:", country);
+
+// Q3:
+var yourName = prompt("Enter Your Name");
+
+// Q4:
+var userName = prompt("Please enter your name:", "John Doe");
+console.log("The entered name is ", userName);
+
+
+// Q6:
+var userResponse = prompt("What is your favorite hobby?", "Reading");
+alert("You entered: " + userResponse);
+
+
+
+                // ----XXX ---- XXXX ---//
+
+
+
+
+
+
+
+
+
+
+
